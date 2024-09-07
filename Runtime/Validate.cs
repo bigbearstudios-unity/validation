cbbf2249721996ab7128536d441544036292cfab
Stuart Farnaby <stuart@bigbearstudios.co.uk>
2024-09-07 11:23:21 +0100

    Added basic validation of Emails, IP Addresses (V4, V6), Strings (Words, Length)

---
+++ Runtime/Validate.cs
@@ -0,0 +1,109 @@
+﻿using System.Text.RegularExpressions;
+
+using BBUnity.Extensions;
+
+namespace BBUnity.Validation {
+    public static class Validate {
+
+        public static bool Email(string toValidate) {
+            return EmailAddress(toValidate);
+        }
+
+
+        public static bool EmailAddress(string toValidate) {
+            Regex r = new Regex(@"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)");
+            return r.IsMatch(toValidate);
+        }
+
+        public static bool URL(string toValidate) {
+            Regex r = new Regex(@"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$");
+            return r.IsMatch(toValidate);
+        }
+
+        /// <summary>
+        /// Fast validation of the ipv4 format
+        /// </summary>
+        /// <param name="toValidate"></param>
+        /// <returns></returns>
+        public static bool IPv4Format(string toValidate) {
+            Regex r = new Regex(@"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$");
+            return r.IsMatch(toValidate);
+        }
+
+        public static bool IPv4(string toValidate) {
+            Regex r = new Regex(@"^([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])$");
+            return r.IsMatch(toValidate);
+        }
+
+        public static bool IPv6(string toValidate) {
+            Regex r = new Regex(@"^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$");
+            return r.IsMatch(toValidate);
+        }
+
+        public static bool NumberOfWords(string str, int minimum, int maximum) {
+            int numberOfWords = str.NumberOfWords();
+
+            if(numberOfWords < minimum) {
+                return false;
+            }
+
+            if(numberOfWords > maximum) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool MinimumNumberOfWords(string str, int minimum) {
+            int numberOfWords = str.NumberOfWords();
+            if(numberOfWords < minimum) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool MaximumNumberOfWords(string str, int maximum) {
+            int numberOfWords = str.NumberOfWords();
+            if(numberOfWords < maximum) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool NumberOfLetters(string str, int minimum, int maximum) {
+            int numberOfLetters = str.Length;
+
+            if(numberOfLetters < minimum) {
+                return false;
+            }
+            
+            if(numberOfLetters > maximum) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool MinimumNumberOfLetters(string str, int minimum) {
+            int numberOfLetters = str.Length;
+
+            if(numberOfLetters < minimum) {
+                return false;
+            }
+            
+            return true;
+        }
+
+        public static bool MaximumNumberOfLetters(string str, int maximum) {
+            int numberOfLetters = str.Length;
+
+            if(numberOfLetters > maximum) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/ValidateTests.cs
@@ -0,0 +1,149 @@
+﻿using NUnit.Framework;
+
+using BBUnity.Validation;
+using BBUnity.TestSupport;
+
+public class ValidateTests {
+
+    [Test]
+    public void IPv4Format_ShouldPassOnValidIps() {
+        UnityAssert.IsTrue(new string[] {
+            "1.1.1.1", 
+            "255.255.255.255"
+        }, Validate.IPv4Format);
+    }
+
+    [Test]
+    public void IPv4Format_ShouldFailOnInvalidIPs() {
+        UnityAssert.IsFalse(new string[] {
+            "a.1.1.1", 
+            "a.b.c.d", 
+            "1-1-1-1"
+        }, Validate.IPv4Format);
+    }
+
+    [Test]
+    public void IPv4_ShouldPassOnValidIps() {
+        UnityAssert.IsTrue(new string[] {
+            "1.1.1.1", 
+            "255.255.255.255"
+        }, Validate.IPv4);
+    }
+
+    [Test]
+    public void IPv4_ShouldFailOnInvalidIPs() {
+        UnityAssert.IsFalse(new string[] {
+            "a.1.1.1", 
+            "a.b.c.d", 
+            "1-1-1-1", 
+            "125.125.125.300"
+        }, Validate.IPv4);
+    }
+
+    [Test]
+    public void IPv6_ShouldPassOnValidIps() {
+        UnityAssert.IsTrue(new string[] {
+            "2001:470:9b36:1::2",
+            "2001:cdba:0000:0000:0000:0000:3257:9652",
+            "2001:cdba:0:0:0:0:3257:9652",
+            "2001:cdba::3257:9652"
+        }, Validate.IPv6);
+    }
+
+    [Test]
+    public void IPv6_ShouldFailOnInvalidIps() {
+        UnityAssert.IsFalse(new string[] {
+            "1200::AB00:1234::2552:7777:1313",
+            "1200:0000:AB00:1234:O000:2552:7777:1313"
+        }, Validate.IPv6);
+    }
+
+    [Test]
+    public void Email_ShouldPassOnValidEmailAddress() {
+        UnityAssert.IsTrue(new string[] {
+            "test@mail.com"
+        }, Validate.Email);
+    }
+
+    [Test]
+    public void EmailAddress_ShouldPassOnValidEmailAddress() {
+        UnityAssert.IsTrue(new string[] {
+            "test@mail.com"
+        }, Validate.EmailAddress);
+    }
+
+    [Test]
+    public void Email_ShouldFailOnInvalidEmailAddress() {
+        UnityAssert.IsFalse(new string[] {
+            "testmail.com", 
+            "@testmail.com", 
+            "test@mailcom"
+        }, Validate.Email);
+    }
+
+    [Test]
+    public void EmailAddress_ShouldFailOnInvalidEmailAddress() {
+        UnityAssert.IsFalse(new string[] {
+            "testmail.com", 
+            "@testmail.com", 
+            "test@mailcom"
+        }, Validate.EmailAddress);
+    }
+
+    [Test]
+    public void URL_ShouldPassOnValidURLs() {
+        UnityAssert.IsTrue(new string[] {
+            "https://testsite.com",
+            "http://testsite.com",
+            "www.testsite.com",
+            "testsite.com",
+            "test.de",
+            "https://github.com/atestproject"
+        }, Validate.URL);
+    }
+
+    [Test]
+    public void URL_ShouldFailOnInvalidURLs () {
+        UnityAssert.IsFalse(new string[] {
+            "testmailcom", 
+            "http:testmailcom"
+        }, Validate.URL);
+    }
+
+    [Test]
+    public void NumberOfWords_ShouldPass_IfMinimumAndMaximumIsMet() {
+        Assert.IsTrue(Validate.NumberOfWords("hello", 1, 1));
+        Assert.IsTrue(Validate.NumberOfWords("hello motto", 2, 2));
+        Assert.IsTrue(Validate.NumberOfWords("hello motto hello", 3, 3));
+        Assert.IsTrue(Validate.NumberOfWords("hello motto hello motto", 4, 4));
+    }
+
+    [Test]
+    public void NumberOfWords_ShouldFail_IfMinimumIsNotMet() {
+        Assert.IsFalse(Validate.NumberOfWords("hello", 2, 3));
+        Assert.IsFalse(Validate.NumberOfWords("hello motto", 3, 3));
+    }
+
+    [Test]
+    public void NumberOfWords_ShouldFail_IfMaximumIsNotMet() {
+        Assert.IsFalse(Validate.NumberOfWords("hello motto", 1, 1));
+        Assert.IsFalse(Validate.NumberOfWords("hello motto yes please", 1, 3));
+    }
+
+    [Test]
+    public void MinimumNumberOfWords_ShouldPass_IfMinimumIsMet() {
+        Assert.IsTrue(Validate.MinimumNumberOfWords("one", 1));
+        Assert.IsTrue(Validate.MinimumNumberOfWords("one two three", 2));
+    }
+
+    [Test]
+    public void MaximumNumberOfWords_ShouldPass_IfMaximumIsMet() {
+        Assert.IsTrue(Validate.MaximumNumberOfWords("one", 1));
+        Assert.IsTrue(Validate.MaximumNumberOfWords("one two three", 3));
+    }
+
+    [Test]
+    public void NumberOfLetters_ShouldPass_IfMinimumAndMaximumIsMet() {
+        Assert.IsTrue(Validate.NumberOfLetters("hello", 1, 5));
+    }
+}
